9928491bdc88a7814d61b5102c45e1d84281d091
beatriz.castro <beatriz.castro@mv.com.br>
2019-07-08 13:31:52 -0300

    Iniciando Typescript II - capitulo 1

--- 01-alurabank/alurabank/app/js/controllers/NegociacaoController.js
+++ 01-alurabank/alurabank/app/js/controllers/NegociacaoController.js
@@ -1,28 +1,42 @@
-import { Negociacao } from '../models/Negociacao';
-import { Negociacoes } from '../models/Negociacoes';
-import { NegociacoesView } from '../views/NegociacoesView';
-import { MensagemView } from '../views/MensagemView';
-export class NegociacaoController {
-    constructor() {
-        this.negociacoes = new Negociacoes();
-        this.negociacoesView = new NegociacoesView('#negociacoes');
-        this.mensagemView = new MensagemView('#mensagemView');
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
-        this.negociacoesView.update(this.negociacoes);
-    }
-    adiciona(event) {
-        event.preventDefault();
-        const negociacao = new Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
-        console.log(`
+System.register(["../models/index", "../views/index"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var index_1, index_2, NegociacaoController;
+    return {
+        setters: [
+            function (index_1_1) {
+                index_1 = index_1_1;
+            },
+            function (index_2_1) {
+                index_2 = index_2_1;
+            }
+        ],
+        execute: function () {
+            NegociacaoController = class NegociacaoController {
+                constructor() {
+                    this.negociacoes = new index_1.Negociacoes();
+                    this.negociacoesView = new index_2.NegociacoesView('#negociacoes');
+                    this.mensagemView = new index_2.MensagemView('#mensagemView');
+                    this._inputData = $('#data');
+                    this._inputQuantidade = $('#quantidade');
+                    this._inputValor = $('#valor');
+                    this.negociacoesView.update(this.negociacoes);
+                }
+                adiciona(event) {
+                    event.preventDefault();
+                    const negociacao = new index_1.Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+                    console.log(`
             Data: ${negociacao.data}
             Quantidade: ${negociacao.quantidade}
             Valor: ${negociacao.valor}
             Volume: ${negociacao.volume}
         `);
-        this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update('Negociação adicionada com sucesso!');
-    }
-}
+                    this.negociacoes.adiciona(negociacao);
+                    this.negociacoesView.update(this.negociacoes);
+                    this.mensagemView.update('Negociação adicionada com sucesso!');
+                }
+            };
+            exports_1("NegociacaoController", NegociacaoController);
+        }
+    };
+});
--- 01-alurabank/alurabank/app/js/views/View.js
+++ 01-alurabank/alurabank/app/js/views/View.js
@@ -1,14 +1,28 @@
-import { Negociacoes } from '../models/Negociacoes';
-export class View {
-    constructor(selector) {
-        this._elemento = $(selector);
-    }
-    update(modelo) {
-        this._elemento.html(this.template(modelo));
-        if (!(modelo instanceof Negociacoes)) {
-            setTimeout(() => {
-                this._elemento.innerHTML = '';
-            }, 1500);
+System.register(["../models/Negociacoes"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Negociacoes_1, View;
+    return {
+        setters: [
+            function (Negociacoes_1_1) {
+                Negociacoes_1 = Negociacoes_1_1;
+            }
+        ],
+        execute: function () {
+            View = class View {
+                constructor(selector) {
+                    this._elemento = $(selector);
+                }
+                update(modelo) {
+                    this._elemento.html(this.template(modelo));
+                    if (!(modelo instanceof Negociacoes_1.Negociacoes)) {
+                        setTimeout(() => {
+                            this._elemento.innerHTML = '';
+                        }, 1500);
+                    }
+                }
+            };
+            exports_1("View", View);
         }
-    }
-}
+    };
+});
